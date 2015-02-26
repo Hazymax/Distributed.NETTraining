3fb8d329ff0f540a37f4020887e2bae37712437b
Max <max>
2015-02-26 11:06:23 +0100

    boucleTestDONE

--- 1-CSharpDiscovery/ControlFlowTests.cs
+++ 1-CSharpDiscovery/ControlFlowTests.cs
@@ -6,47 +6,74 @@ namespace CSharpDiscovery
     [TestFixture]
     public class ControlFlowTests
     {
-        //[Test]
-        //public void UseForToConcatenateStringArrayValues()
-        //{
-        //    var stringArray = new[] { "plip", "plop", "plup" };
-        //    // concatenate string array values in a single string with the simplest solution using a for, then refactor (but keep for loop) using StringBuilder (+ avoid "", use string.Empty instead) for memory usage reason
+        [Test]
+        public void UseForToConcatenateStringArrayValues()
+        {
+            var stringArray = new[] { "plip", "plop", "plup" };
+            // concatenate string array values in a single string with the simplest solution using a for, then refactor (but keep for loop) using StringBuilder (+ avoid "", use string.Empty instead) for memory usage reason
+            var stringConcatenated = "";
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                stringConcatenated = stringConcatenated + stringArray[i];
+            }
+            Check.That(stringConcatenated).Equals("plipplopplup");
+        }
 
-        //    Check.That(stringConcatenated).Equals("plipplopplup");
-        //}
+        [Test]
+        public void UseForeachToConcatenateStringArrayValues()
+        {
+            var stringConcatenated = "";
+            var stringArray = new[] { "plip", "plop", "plup" };
+            foreach (var str in stringArray)
+            {
+                stringConcatenated = stringConcatenated + str;
+            }
 
-        //[Test]
-        //public void UseForeachToConcatenateStringArrayValues()
-        //{
-        //    var stringArray = new[] { "plip", "plop", "plup" };
-        //    // concatenate string array values in a single string with the simplest solution using a foreach
+            // concatenate string array values in a single string with the simplest solution using a foreach
 
-        //    Check.That(stringConcatenated).Equals("plipplopplup");
-        //}
+            Check.That(stringConcatenated).Equals("plipplopplup");
+        }
 
-        //[Test]
-        //public void UseWhileToConcatenateStringArrayValues()
-        //{
-        //    var stringArray = new[] { "plip", "plop", "plup" };
-        //    // concatenate string array values in a single string with the simplest solution using a while
+        [Test]
+        public void UseWhileToConcatenateStringArrayValues()
+        {
+            var stringArray = new[] { "plip", "plop", "plup" };
+            int i =0;
+            var stringConcatenated = "";
+            // concatenate string array values in a single string with the simplest solution using a while
+            while (i < stringArray.Length)
+            {
+                stringConcatenated = stringConcatenated + stringArray[i++];
+            }
+            Check.That(stringConcatenated).Equals("plipplopplup");
+        }
 
-        //    Check.That(stringConcatenated).Equals("plipplopplup");
-        //}
+        [Test]
+        public void UseDoWhileToConcatenateStringArrayValues()
+        {
+            var stringArray = new[] { "plip", "plop", "plup" };
+            // concatenate string array values in a single string with the simplest solution using a foreach
+            int i = 0;
+            var stringConcatenated = "";
+            do
+            {
+                stringConcatenated = stringConcatenated + stringArray[i];
+            } while (i < stringArray.Length);
 
-        //[Test]
-        //public void UseDoWhileToConcatenateStringArrayValues()
-        //{
-        //    var stringArray = new[] { "plip", "plop", "plup" };
-        //    // concatenate string array values in a single string with the simplest solution using a foreach
-            
-        //    Check.That(stringConcatenated).Equals("plipplopplup");
-        //}
+            Check.That(stringConcatenated).Equals("plipplopplup");
+        }
 
         //[Test]
         //public void UseIfElseElseIfDuringConcatenationOfStringArrayValues()
         //{
         //    var stringArray = new[] { "plip", "plop", "plup", "foo" };
         //    // concatenate a string, with "good, " when item is plip, "not so good, " when item starts with "pl", "bad, " in any other case
+        //    int i = 0;
+        //    var stringConcatenated = "";
+        //    if (i < stringArray.Length){
+        //        stringConcatenated = stringConcatenated + stringArray[i++];
+        //    }
+
         //    Check.That(stringConcatenated).Equals("good, not so good, not so good, bad, ");
         //}
 
