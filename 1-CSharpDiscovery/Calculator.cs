dffe9288948496cc9dafa46fad66c74db40cb13c
Max <max>
2015-02-26 11:45:22 +0100

    finCours1

---
+++ 1-CSharpDiscovery/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDiscovery
+{
+    class Calculator
+    {
+        public string Name { get; set; }
+
+        public Calculator()
+        {
+            this.Name = "Calculator";
+        }
+
+        public double sumOfTheArray(double[] tab)
+        {
+            double sum = 0;
+            foreach (double f in tab)
+            {
+                sum = sum + f;
+            }
+            return sum;
+        }
+
+        public double onePlusTwo(String str)
+        {
+            string[] nb = str.Split('+');
+            double sum = 0;
+            foreach (String element in nb)
+            {
+                if (element == "pi"){sum = sum + 3.14;}
+                else {sum = sum + double.Parse(element);}
+            }
+            return sum;
+        }
+
+    }
+
+    
+}
--- 1-CSharpDiscovery/ClassesTests.cs
+++ 1-CSharpDiscovery/ClassesTests.cs
@@ -6,61 +6,70 @@ namespace CSharpDiscovery
     [TestFixture]
     public class ClassesTests
     {
-        //[Test]
-        //public void CreateACalculatorClassWithADefaultConstructorAndInstanciate()
-        //{
-        //    Check.That(calculator).IsNotNull();
-        //}
+        [Test]
+        public void CreateACalculatorClassWithADefaultConstructorAndInstanciate()
+        {
+            Calculator calculator = new Calculator();
+            Check.That(calculator).IsNotNull();
+        }
 
-        //[Test]
-        //public void AddAnotherConstructorWithAFriendlyNameAndInstanciate()
-        //{
-        //    // use a public member for Name for now, i.e public string Name;
-        //    Check.That(calculator.Name).Equals("Calculator");
-        //}
+        [Test]
+        public void AddAnotherConstructorWithAFriendlyNameAndInstanciate()
+        {
+            // use a public member for Name for now, i.e public string Name;
+            Calculator calculator = new Calculator();
+            Check.That(calculator.Name).Equals("Calculator");
+        }
 
-        //[Test]
-        //public void AddAMethodThatMakeASumOfAnArrayOfDouble()
-        //{
-        //    var valuesToSum = new[] { 1.3, 1.7 };
-        //    // add a method Sum to calculator class
-        //    Check.That(sumOfTheArray).Equals(3.0);
-        //}
+        [Test]
+        public void AddAMethodThatMakeASumOfAnArrayOfDouble()
+        {
+            var valuesToSum = new[] { 1.3, 1.7 };
+            // add a method Sum to calculator class
+            Calculator calculator = new Calculator();
+            Check.That(calculator.sumOfTheArray(valuesToSum)).Equals(3.0);
+        }
 
-        //[Test]
-        //public void AddAMethodOverloadThatMakeASumOfTwoDoubleFromStringRepresentation()
-        //{
-        //    var sumOfTwoDoubleFromString = "1,0+2";
-        //    // add a method with the same name that uses the previous method
-        //    // tips : use string.Split
-        //    Check.That(onePlusTwo).Equals(3.0);
-        //}
+        [Test]
+        public void AddAMethodOverloadThatMakeASumOfTwoDoubleFromStringRepresentation()
+        {
+            var sumOfTwoDoubleFromString = "1,0+2";
+            // add a method with the same name that uses the previous method
+            // tips : use string.Split
+            Calculator calculator = new Calculator();
+            Check.That(calculator.onePlusTwo(sumOfTwoDoubleFromString)).Equals(3.0);
+        }
 
-        //[Test]
-        //public void AddAGetterForNameInsteadOfPublicNameMember()
-        //{
-        //    Check.That(calculator.Name).Equals("Calculator");
-        //}
+        [Test]
+        public void AddAGetterForNameInsteadOfPublicNameMember()
+        {
+            Calculator calculator = new Calculator();
+            Check.That(calculator.Name).Equals("Calculator");
+        }
 
-        //[Test]
-        //public void AddASetterForNameAndChangeNameWithIt()
-        //{
-        //    Check.That(calculator.Name).Equals("Enhanced Calculator");
-        //}
+        [Test]
+        public void AddASetterForNameAndChangeNameWithIt()
+        {
+            Calculator calculator = new Calculator();
+            calculator.Name = "Enhanced Calculator";
+            Check.That(calculator.Name).Equals("Enhanced Calculator");
+        }
 
-        //[Test]
-        //public void UseObjectInitilizerWithDefaultConstructor()
-        //{
-        //    Check.That(calculator.Name).Equals("Calculator");
-        //}
+        [Test]
+        public void UseObjectInitilizerWithDefaultConstructor()
+        {
+            Calculator calculator = new Calculator();
+            Check.That(calculator.Name).Equals("Calculator");
+        }
 
-        //[Test]
-        //public void DefineConstantForPi()
-        //{
-        //    var sumOfADoubleAndPiConstant = "1,2 + pi";
-        //    // define pi constant (as double) and replace pi string with constant value
-        //    Check.That(sum).Equals(4.34);
-        //}
+        [Test]
+        public void DefineConstantForPi()
+        {
+            var sumOfADoubleAndPiConstant = "1,2 + pi";
+            // define pi constant (as double) and replace pi string with constant value
+            Calculator calculator = new Calculator();
+            Check.That(calculator.onePlusTwo(sumOfADoubleAndPiConstant)).Equals(4.34);
+        }
 
         //[Test]
         //public void StaticReadonlyMembers()
@@ -84,4 +93,8 @@ namespace CSharpDiscovery
         //    Check.That(staticCalculator.Name).Equals("Static calculator");
         //}
     }
+
+
+
+    
 }
