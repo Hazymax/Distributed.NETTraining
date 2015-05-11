16e4945713aa058cdaded5cd87c81ead59929b95
Max <max>
2015-05-11 15:18:43 +0200

    Merge tag 'TD1.3.2' into Branche_TP1
    Conflicts:
    	1-CSharpDiscovery/1-CSharpDiscovery.csproj
    
    Signed-off-by: Max <max>

---
+++ 2-CQRSTwitterLike/Messaging/TimelineMessage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    public struct TimelineMessage
+    {
+        private readonly UserId _ownerID;
+        private readonly DateTime _date;        
+        private readonly int _nb_favoris;
+        private readonly int _nb_retweets;
+        private readonly String _content;
+
+        public TimelineMessage(UserId owner,DateTime date, int nbfavoris, int nbretweet,String content) {
+            this._ownerID = owner;
+            this._date = date;
+            this._nb_favoris = nbfavoris;
+            this._nb_retweets = nbretweet;
+            this._content = content;
+        }
+
+        public UserId OwnerId
+        {
+            get { return _ownerID; }
+        }
+
+        public int NbFavoris
+        {
+            get { return _nb_favoris; }
+        }
+
+        public int NbRetweets
+        {
+            get { return _nb_retweets; }
+        }
+
+        public String Content
+        {
+            get { return _content; }
+        }
+
+    }
+}
---
+++ 2-CQRSTwitterLike/Messaging/UserId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    public class UserId
+    {
+        private readonly int ID;
+
+        public int Id
+        {
+            get { return ID; }
+        }
+
+        public UserId(int id)
+        {
+            ID = id;
+        }
+    }
+}
